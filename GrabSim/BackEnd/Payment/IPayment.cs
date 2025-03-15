456458bbf13291a819a7d47524cf328910c32b5e
trinhthaominh <92960404+trinhthaominh@users.noreply.github.com>
2025-03-15 08:30:49 +0700

    Uploade Payment

---
+++ GrabSim/BackEnd/Payment/IPayment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Project.Payment
+{
+    public delegate void PaymentProcessedEventHandler(object sender, PaymentEventArgs e);
+    public interface IPayment
+    {
+        bool Status { get; set; }
+        bool ProcessPayment(uint amount);
+        string GetPaymentMethod();
+        event PaymentProcessedEventHandler PaymentProcessed;
+    }
+}
---
+++ GrabSim/BackEnd/Payment/OnlinePayment.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Project.Payment
+{
+    public class OnlinePayment : IPayment
+    {
+        public bool Status { get; set; }
+        public event PaymentProcessedEventHandler PaymentProcessed;
+
+        public OnlinePayment(bool status)
+        {
+            Status = status;
+        }
+
+        public bool ProcessPayment(uint amount)
+        {
+            bool result = Status;
+            string message = result ? "Thanh toán thành công" : "Giao dịch thất bại! Xin vui lòng thử lại";
+            OnPaymentProcessed(new PaymentEventArgs(result, amount, message, GetPaymentMethod()));
+            return result;
+        }
+
+        public string GetPaymentMethod()
+        {
+            return "Thanh toán trực tuyến";
+        }
+
+        protected virtual void OnPaymentProcessed(PaymentEventArgs e)
+        {
+            PaymentProcessed?.Invoke(this, e);
+        }
+    }
+}
